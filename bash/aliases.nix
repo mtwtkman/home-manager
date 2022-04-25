e6b2d89407156b07cee73c0d58a32727de23f5c4
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-04-25 22:01:42 +0900

    safe rm

--- bash/aliases.nix
+++ bash/aliases.nix
@@ -2,10 +2,7 @@
   ll = "ls -alF";
   la = "ls -A";
   l = "ls -CF";
-  "rmr!" = "rm -rf";
-  "rm!" = "rm -f";
-  rm = "rm -i";
-  rmr = "rm -ri";
+  rm = "trash-put";
   hm = "home-manager";
   update = "sudo apt update && sudo apt upgrade -y && sudo apt autoremove";
 }
