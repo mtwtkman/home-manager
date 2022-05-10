afd54962fb453c8729d1940ceb8325b23406337d
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-05-11 07:40:51 +0900

    benri alias

--- bash/aliases.nix
+++ bash/aliases.nix
@@ -6,6 +6,7 @@
   hm = "home-manager --impure";
   update = "sudo apt update && sudo apt upgrade -y && sudo apt autoremove";
   hf = "history | fzf";
+  dev = "nix develop";
   nvim = "nvim --listen /tmp/nvimsocket";
   vim = "nvim";
 }
