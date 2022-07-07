9d2fccfaef754fee48b5b53ad607a1440b9efd60
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-07-07 18:56:51 +0900

    flake update

--- flake.nix
+++ flake.nix
@@ -2,21 +2,24 @@
   description = "My home-manager configuration";
 
   inputs = {
-    home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { home-manager, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       meta = import ./meta.nix;
+      system = meta.system;
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations.${meta.username} = home-manager.lib.homeManagerConfiguration {
-        configuration = import ./home.nix;
-        system = meta.system;
-        username = meta.username;
-        homeDirectory = meta.home;
-        stateVersion = meta.homeManagerStateVersion;
+        inherit pkgs;
+        modules = [
+        ./home.nix
+        ];
       };
     };
 }
