2f17fc5e42f4183a69cbe94c651ee498479b0469
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-11-13 20:21:36 +0900

    bump pura version

--- packages/pura.nix
+++ packages/pura.nix
@@ -5,6 +5,6 @@ import
     owner = "mtwtkman";
     repo = "pura";
     rev = "HEAD";
-    sha256 = "1q91idg3a6ms3xnnr93qk5sia0v1n7xpx8n5y3g3zk5k8a3jpgif";
+    sha256 = "1kfckla6znzivag9l5hhaqhb664zy501w47h4xlg1gjqzx8j0dfz";
   })
 { }
