6eecc44c386842daedcb63463c2bded65fc81f12
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-01-16 17:11:11 +0900

    works fine

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -28,14 +28,14 @@ cmp.setup({
   },
 })
 
-cmd.setup.cmdline({ "/", "?" }, {
+cmp.setup.cmdline({ "/", "?" }, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = "buffer" },
   },
 })
 
-cmd.setup.cmdline(":", {
+cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "path" },
