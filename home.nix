e0341520e84e52b1aa1484b07a8704be371c26e6
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-01-27 20:43:28 +0900

    use sessionVariables

--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@ in
   home.stateVersion = meta.homeManagerStateVersion;
   home.sessionVariables = {
     NIX_CONF_DIR = meta.nixConfigDirectory;
+    FZF_DEFAULT_OPTS="--reverse --extended --multi --inline-info --prompt=\"fzf>\""; # Because programs.fzf.defaultOptions never work.
   };
   home.packages = with pkgs; [
     ripgrep
@@ -44,13 +45,6 @@ in
       enableZshIntegration = false;
       enableFishIntegration = false;
       defaultCommand = defaultCommand;
-      # defaultOptions = [  # NOTE: never not activate
-      #   "--reverse"
-      #   "--extended"
-      #   "--multi"
-      #   "--inline-info"
-      #   "--prompt=\"fzf>\""
-      # ];
       fileWidgetCommand = defaultCommand;
     };
 
