4970c8d11cab32e855cadaa2298b7f1ffbf8337f
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-02-02 13:08:48 +0900

    cleanup lsp keymap

--- nvim/lua/plugins/nvim-lspconfig/init.lua
+++ nvim/lua/plugins/nvim-lspconfig/init.lua
@@ -4,7 +4,6 @@ local nmap = utils.nmap
 nmap("<space>e", vim.diagnostic.open_float, { silent = true })
 nmap("[d", vim.diagnostic.goto_prev, { silent = true })
 nmap("]d", vim.diagnostic.goto_next, { silent = true })
-nmap("<space>q", vim.diagnostic.setloclist, { silent = true })
 
 local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
@@ -21,7 +20,6 @@ local on_attach = function(client, bufnr)
   nmap("<space>D", vim.lsp.buf.type_definition, { silent = true, buffer = bufnr })
   nmap("<space>rn", vim.lsp.buf.rename, { silent = true, buffer = bufnr })
   nmap("<space>ca", vim.lsp.buf.code_action, { silent = true, buffer = bufnr })
-  nmap("gr", vim.lsp.buf.references, { silent = true, buffer = bufnr })
   nmap("<space>f", function() vim.lsp.buf.format { async = true } end, { silent = true, buffer = bufnr })
 end
 
