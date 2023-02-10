1ca9eca5825fdc9802cec4d0c0a7ccf1741b47f3
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-02-10 20:08:23 +0900

    avoid C-space

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -11,7 +11,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-space>"] = cmp.mapping.complete(),
+    ["<C-o>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
   }),
