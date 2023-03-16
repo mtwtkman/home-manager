42b78ad82c2440d05a2c6d52c33d327e523735a5
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-03-16 22:45:40 +0900

    added link nix.conf step

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 config_path="${HOME}/.config"
+nix_dir="${config_path}/nix"
 my_home_manager_path="${config_path}/home-manager"
 
 echo "arrange the config directory"
 [ ! -d "${config_path}" ] && mkdir ${config_path}
+
+echo "arrange the nix directory"
+[ ! -d "${nix_dir}" ] && mkdir ${nix_dir}
 
 echo 'install nix'
 # ref: https://nixos.org/download.html
@@ -22,6 +26,10 @@ nix-shell '<home-manager>' -A install
 echo 'install repo'
 rm -rf ${my_home_manager_path}
 nix-shell -p git --run "git clone git@github.com:mtwtkman/home-manager ${my_home_manager_path}"
+
+echo 'link nix.conf'
+nix_conf="nix.conf"
+ln ${my_home_manager_path}/${nix_conf} ${nix_dir}/${nix_conf}
 
 echo 'activate home-manager'
 nix-shell -p git --run "HOME_MANAGER_ROOT=${my_home_manager_path} home-manager --impure switch"
