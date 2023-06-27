75a05b12c8a8725ca23d79d0b440969336d1261a
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-06-27 21:34:22 +0900

    setting dap

--- nvim/lua/plugins/nvim-dap-ui.lua
+++ nvim/lua/plugins/nvim-dap-ui.lua
@@ -1,7 +1,12 @@
-require("dapui").setup()
-
-local dap = require("dap")
+local nmap = require("utils").nmap
 local dapui = require("dapui")
+local dap = require("dap")
+
+dapui.setup({
+  controls = {
+    enabled = false,
+  },
+})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
@@ -14,3 +19,6 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
+
+nmap("<leader>df", function() dapui.float_element("stacks", { enter = true }) end, { silent = true })
+nmap("<leader>ds", function() dapui.float_element("scopes", { enter = true }) end, { silent = true })
--- nvim/lua/plugins/nvim-dap/init.lua
+++ nvim/lua/plugins/nvim-dap/init.lua
@@ -1,19 +1,13 @@
 local utils = require("utils")
 local dap = require("dap")
-local ui = require("dap.ui.widgets")
 local nmap = utils.nmap
 
-nmap("<leader>dc", dap.continue, { silent = true })
-nmap("<leader>do", dap.step_over, { silent = true })
-nmap("<leader>di", dap.step_into, { silent = true })
-nmap("<leader>dj", dap.step_out, { silent = true })
+nmap("<F5>", dap.continue, { silent = true })
+nmap("<F2>", dap.step_over, { silent = true })
+nmap("<F3>", dap.step_into, { silent = true })
+nmap("<F4>", dap.step_out, { silent = true })
 nmap("<leader>db", dap.toggle_breakpoint, { silent = true })
-nmap("<leader>dr", dap.repl.open, { silent = true })
 nmap("<leader>dl", dap.run_last, { silent = true })
-nmap("<leader>dh", ui.hover, { silent = true })
-nmap("<leader>dp", ui.preview, { silent = true })
-nmap("<leader>df", function() ui.centered_float(ui.frames) end, { silent = true })
-nmap("<leader>ds", function() ui.centered_float(ui.scopes) end, { silent = true })
 
 local load_setting = function(langtype)
   local config = require("plugins.nvim-dap." .. langtype)
