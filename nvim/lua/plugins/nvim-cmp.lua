e14b279b45d88e504e0e339359237fdf680701e8
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-07-08 20:42:22 +0900

    added nvim-cmp setting for luasnip

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -15,6 +15,11 @@ set.completeopt = {
 }
 
 cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
