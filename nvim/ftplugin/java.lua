98013be4daa4cb6536c69289b4347b7f97a8411c
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-07-18 18:05:51 +0900

    setup dap for jdtls

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,11 +1,16 @@
-local root_markers = {'gradlew', '.git'}
+local jdtls = require("jdtls")
+local root_markers = { 'gradlew', '.git' }
 local root_dir = require('jdtls.setup').find_root(root_markers)
 local home = os.getenv('HOME')
 local workspace = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
 local cmd = { "jdt-language-server", "-data", workspace }
 local config = require("lsp").make_default_config()
+local bundles = {}
 
 config.cmd = cmd
 config.root_dir = root_dir
-require("jdtls").start_or_attach(config)
+config.init_options = {
+  bundles = bundles,
+}
+jdtls.start_or_attach(config)
