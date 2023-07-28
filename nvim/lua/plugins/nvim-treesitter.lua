e77b62fe575cecd7b0d568cd8723e38c20099e23
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-07-28 21:45:54 +0900

    setup nvim-treesitter-textobject

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -5,9 +5,22 @@ require("nvim-treesitter.configs").setup({
   textobjects = {
     select = {
       enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
     },
     swap = {
       enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
     },
     move = {
       enable = true,
