69241831ad9b3195e6c48f5f2cf1abc1a9aa1d55
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-08-08 19:24:09 +0900

    improved setup.sh

--- setup.sh
+++ setup.sh
@@ -1,37 +1,51 @@
 #!/bin/sh
+BLUE='\033[0;34m'
+NC='\033[0m'
 
 config_path="${HOME}/.config"
 nix_dir="${config_path}/nix"
 my_home_manager_path="${config_path}/home-manager"
 
-echo "arrange the config directory"
-[ ! -d "${config_path}" ] && mkdir ${config_path}
+function pp() {
+  echo -e "${BLUE}[setup.sh] ${1}${NC}"
+}
 
-echo "arrange the nix directory"
-[ ! -d "${nix_dir}" ] && mkdir ${nix_dir}
+if [ ! -d "${config_path}" ]; then
+  pp "create ${config_path}"
+  mkdir ${config_path}
+fi
 
-echo 'install nix'
+if [ ! -d "${nix_dir}" ]; then
+  pp "create ${nix_dir}"
+  mkdir ${nix_dir}
+fi
+
+pp 'install nix'
 # ref: https://nixos.org/download.html
 curl -L https://nixos.org/nix/install | sh
 
 . $HOME/.nix-profile/etc/profile.d/nix.sh
 
-echo 'install home-manager'
+pp 'install home-manager'
 # ref: https://nix-community.github.io/home-manager/index.html#sec-install-standalone
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
-echo 'install repo'
+pp 'install repo'
 rm -rf ${my_home_manager_path}
 nix-shell -p git --run "git clone git@github.com/mtwtkman/home-manager ${my_home_manager_path}"
 
-echo 'link nix.conf'
 nix_conf="nix.conf"
+if [ ! -e ${nix_conf}  ]; then
+  pp "remove obsolete symlink"
+  rm ${nix_conf}
+fi
+pp 'link nix.conf'
 ln ${my_home_manager_path}/${nix_conf} ${nix_dir}/${nix_conf}
 
-echo 'activate home-manager'
+pp 'activate home-manager'
 nix-shell -p git --run "NIX_HOME_MANAGER_ROOT=${my_home_manager_path} home-manager --impure switch"
 
-echo 'done'
+pp 'done'
