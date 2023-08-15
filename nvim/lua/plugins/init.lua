436696ad6f2480aedf902f7d13e85258a1ece7c4
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-08-15 14:42:10 +0900

    lazy

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -26,9 +26,16 @@ local plugins = {
     config = function()
       require("plugins.nvim-treesitter")
     end,
+    event = "VeryLazy",
   },
-  "nvim-treesitter/nvim-treesitter-context",
-  "nvim-treesitter/nvim-treesitter-textobjects",
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+  },
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = {
@@ -43,9 +50,16 @@ local plugins = {
     config = function()
       require("plugins.nvim-lspconfig")
     end,
+    event = "VeryLazy",
   },
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-path",
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    event = "VeryLazy",
+  },
+  {
+    "hrsh7th/cmp-path",
+    event = "VeryLazy",
+  },
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -54,27 +68,40 @@ local plugins = {
     config = function()
       require("plugins.nvim-cmp")
     end,
+    event = "VeryLazy",
   },
-  "saadparwaiz1/cmp_luasnip",
+  {
+    "saadparwaiz1/cmp_luasnip",
+    event = "VeryLazy",
+  },
   {
     "L3MON4D3/LuaSnip",
     config = function()
       require("plugins.LuaSnip")
     end,
+    event = "VeryLazy",
   },
-  "hrsh7th/cmp-cmdline",
-  "hrsh7th/cmp-buffer",
+  {
+    "hrsh7th/cmp-cmdline",
+    event = "VeryLazy",
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    event = "VeryLazy",
+  },
   {
     "rcarriga/cmp-dap",
     dependencies = {
       "mfussenegger/nvim-dap",
     },
+    event = "VeryLazy",
   },
   {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
       require("plugins.indent-blankline")
     end,
+    event = "VeryLazy",
   },
   {
     "ibhagwan/fzf-lua",
@@ -84,12 +111,14 @@ local plugins = {
     config = function()
       require("plugins.fzf-lua")
     end,
+    event = "VeryLazy",
   },
   {
     "mfussenegger/nvim-dap",
     config = function()
       require("plugins.nvim-dap")
     end,
+    event = "VeryLazy",
   },
   {
     "rcarriga/nvim-dap-ui",
@@ -99,43 +128,49 @@ local plugins = {
     config = function()
       require("plugins.nvim-dap-ui")
     end,
+    event = "VeryLazy",
   },
   {
     "folke/flash.nvim",
-    event = "VeryLazy",
     config = function()
       require("plugins.flash")
     end,
+    event = "VeryLazy",
   },
   {
     "simrat39/symbols-outline.nvim",
     config = function()
       require("plugins.symbols-outline")
     end,
+    event = "VeryLazy",
   },
   {
     "https://gitlab.com/yorickpeterse/nvim-window",
     config = function()
       require("plugins.nvim-window")
     end,
+    event = "VeryLazy",
   },
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("plugins.gitsigns")
     end,
+    event = "VeryLazy",
   },
   {
     "SmiteshP/nvim-navic",
     dependencies = {
       "neovim/nvim-lspconfig",
     },
+    event = "VeryLazy",
   },
   {
     "numToStr/Comment.nvim",
     config = function()
       require("plugins.Comment")
-    end
+    end,
+    event = "VeryLazy",
   },
   {
     "sindrets/diffview.nvim",
@@ -144,13 +179,15 @@ local plugins = {
     },
     config = function()
       require("plugins.diffview")
-    end
+    end,
+    event = "VeryLazy",
   },
   {
     "chentoast/marks.nvim",
     config = function()
       require("plugins.marks")
     end,
+    event = "VeryLazy",
   },
   {
     "NeogitOrg/neogit",
@@ -159,9 +196,13 @@ local plugins = {
     },
     config = function()
       require("plugins.neogit")
-    end
+    end,
+    event = "VeryLazy",
   },
-  "mfussenegger/nvim-jdtls",
+  {
+    "mfussenegger/nvim-jdtls",
+    event = "VeryLazy",
+  },
   {
     "utilyre/barbecue.nvim",
     dependencies = {
@@ -171,6 +212,7 @@ local plugins = {
     config = function()
       require("plugins.barbecue")
     end,
+    event = "VeryLazy",
   },
 }
 
