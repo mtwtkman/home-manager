cfc8149983d297504e4fc882418055f464848635
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-08-23 14:52:28 +0900

    removed obsoleted setting

--- nvim/ftplugin/haskell.lua
+++ nvim/ftplugin/haskell.lua
@@ -6,18 +6,4 @@ vim.g.haskell_tools = {
       return lsp_config.on_attach(client, bufnr)
     end,
   },
-  -- dap = {
-  --   type = "haskell",
-  --   request = "launch",
-  --   name = "Debug",
-  --   workspace = "${workspaceFolder}",
-  --   startup = "${file}",
-  --   stopOnEntry = true,
-  --   logFile = vim.fn.stdpath("data") .. "/haskell-dap.log",
-  --   logLevel = "DEBUG",
-  --   ghciEnv = vim.empty_dict(),
-  --   ghciPrompt = "λ: ",
-  --   ghciInitialPrompt = "ghci> ",
-  --   ghciCmd= "cabal exec -- ghci-dap --interactive -i ${workspaceFolder}",
-  -- },
 }
