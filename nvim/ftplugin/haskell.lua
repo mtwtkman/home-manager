c88f478f4d6d0ba80cc3c8ce4ff2f2928bff0c18
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-08-23 14:45:40 +0900

    rollback dap setting for haskell

--- nvim/ftplugin/haskell.lua
+++ nvim/ftplugin/haskell.lua
@@ -5,5 +5,19 @@ vim.g.haskell_tools = {
     on_attach = function(client, bufnr, _)
       return lsp_config.on_attach(client, bufnr)
     end,
-  }
+  },
+  -- dap = {
+  --   type = "haskell",
+  --   request = "launch",
+  --   name = "Debug",
+  --   workspace = "${workspaceFolder}",
+  --   startup = "${file}",
+  --   stopOnEntry = true,
+  --   logFile = vim.fn.stdpath("data") .. "/haskell-dap.log",
+  --   logLevel = "DEBUG",
+  --   ghciEnv = vim.empty_dict(),
+  --   ghciPrompt = "λ: ",
+  --   ghciInitialPrompt = "ghci> ",
+  --   ghciCmd= "cabal exec -- ghci-dap --interactive -i ${workspaceFolder}",
+  -- },
 }
