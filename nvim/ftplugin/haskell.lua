94f960cef0acb4a40368b8dc23e6de22d15f3e93
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-08-23 11:15:07 +0900

    use haskell-tools

---
+++ nvim/ftplugin/haskell.lua
@@ -0,0 +1,9 @@
+local lsp_config = require("lsp").make_default_config()
+
+vim.g.haskell_tools = {
+  hls = {
+    on_attach = function(client, bufnr, _)
+      return lsp_config.on_attach(client, bufnr)
+    end,
+  }
+}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -222,6 +222,14 @@ local plugins = {
     end,
     evetn = "VeryLazy",
   },
+  {
+    "mrcjkb/haskell-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    branch = "2.x.x",
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+  },
 }
 
 local opts = {
