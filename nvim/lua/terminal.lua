d5d358813dc7d666fc7f7f262abfd322522cc714
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-10-23 10:41:06 +0900

    updated flake

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -2,12 +2,13 @@ local utils = require("utils")
 local tmap = utils.tmap
 local nmap = utils.nmap
 
+nmap("<leader>tt", ":tabe<CR>:terminal<CR>", { silent = true })
+nmap("<leader>tv", ":vsp<CR>:terminal<CR>", { silent = true })
+
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0, silent = true }
   tmap("<ESC>", [[<C-\><C-n>]], opts)
   tmap("<C-w>", [[<C-\><C-n><C-w>]], opts)
-  nmap("<leader>tt", ":tabe<CR>:terminal<CR>", { silent = true })
-  nmap("<leader>tv", ":vsp<CR>:terminal<CR>", { silent = true })
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
