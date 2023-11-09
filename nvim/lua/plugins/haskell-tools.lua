ae9e092a8c36f832a2505628ded255ef5ad16b34
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-11-09 15:35:34 +0900

    setup dap for haskell

--- nvim/lua/plugins/haskell-tools.lua
+++ nvim/lua/plugins/haskell-tools.lua
@@ -5,7 +5,6 @@ local lsp_config = require("lsp").make_default_config()
 vim.g.haskell_tools = {
   hls = {
     on_attach = function(client, bufnr, ht)
-      print("haskell tools on_attach!")
       nmap("<space>hs", ht.hoogle.hoogle_signature, { silent = true })
       nmap("<space>ea", ht.lsp.buf_eval_all, { silent = true })
       return lsp_config.on_attach(client, bufnr)
