8a7a26f3619dffdece672761a347702133ea1ba8
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-02-01 15:39:30 +0900

    updated java ftplugin

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -4,7 +4,7 @@ local root_dir = require('jdtls.setup').find_root(root_markers)
 local home = os.getenv('HOME')
 local workspace = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
-local cmd = { "jdt-language-server", "-data", workspace }
+local cmd = { "jdtls", "-data", workspace }
 local config = require("lsp").make_default_config()
 local bundles = {}
 
