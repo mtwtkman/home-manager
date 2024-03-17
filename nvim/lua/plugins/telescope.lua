f0c6d902738101b852c5a1dea6a7698d5232c0f7
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-03-17 10:19:05 +0900

    telescope keymap setting

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,11 +2,6 @@ local utils = require("utils")
 local nmap = utils.nmap
 local telescope = require("telescope")
 
-local builtin = require('telescope.builtin')
-nmap('<leader>o', builtin.find_files, {})
-nmap('<leader>ff', builtin.live_grep, {})
-nmap('<leader>b', builtin.buffers, {})
-
 telescope.setup({
   extensions = {
     fzf = {
@@ -18,6 +13,21 @@ telescope.setup({
   }
 })
 
-telescope("telescope").load_extension("fzf")
+telescope.load_extension("fzf")
 
+local builtin = require('telescope.builtin')
+nmap('<leader>o', builtin.find_files, { silent = true })
+nmap('<leader>ff', builtin.live_grep, { silent = true })
+nmap('<leader>b', builtin.buffers, { silent = true })
+nmap('<leader>hh', builtin.oldfiles, { silent = true })
+nmap('<leader>h:', builtin.command_history, { silent = true })
+nmap('<leader>h/', builtin.search_history, { silent = true })
+nmap('<C-p>', builtin.commands, { silent = true })
+nmap('<leader>/', builtin.current_buffer_fuzzy_find, { silent = true })
+nmap('<space>q', builtin.diagnostics, { silent = true })
+nmap('gr', builtin.lsp_references, { silent = true })
+
+vim.api.nvim_create_user_command("Maps", builtin.keymaps, {})
+vim.api.nvim_create_user_command("GitCommits", builtin.git_commits, {})
+vim.api.nvim_create_user_command("GitBranches", builtin.git_branches, {})
 
