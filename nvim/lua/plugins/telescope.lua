18583914370f4608b3085ead83bf5ba8bc005d28
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-03-17 12:10:21 +0900

    perfect

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,6 +5,9 @@ local telescope = require("telescope")
 telescope.setup({
   defaults = {
     sorting_strategy = "ascending",
+    layout_config = {
+      prompt_position = "top",
+    },
   },
   extensions = {
     fzf = {
