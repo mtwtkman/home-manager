cb4baadb04e44a7a98e7ebc1cd5f42c10f716950
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-03-20 11:53:06 +0900

    updated dapui

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -101,6 +101,7 @@ local plugins = {
     "rcarriga/nvim-dap-ui",
     dependencies = {
       "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
     },
     config = function()
       require("plugins.nvim-dap-ui")
